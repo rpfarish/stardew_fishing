a479494bfd4bc07191aa9a93af37b1f16e8e331a
rpfar <rpfarish@live.com>
2025-02-20 16:17:05 -0500

    added json and button

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import "./App.css";
 import GridLayout from "./GridLayout";
 import Footer from "./Footer";
+import Interface from "./Interface";
 
 function App() {
   return (
     <>
       <div className="wrapper">
+        <Interface />
+
         <GridLayout />
       </div>
       <Footer />
---
+++ src/Interface.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import fishData from "./fish.json";
+
+console.log(fishData["Name"]);
+
+const Interface = () => {
+  const [count, setCount] = useState(0);
+  const [curSeason, setCurSeason] = useState("Spring");
+  const allSeasons = ["Spring", "Summer", "Fall", "Winter"];
+  return (
+    <>
+      <div className="outer-interface">
+        <div className="interface">
+          <p>Current Season {curSeason}</p>
+          <div className="fish-names">
+            {Object.values(fishData)
+              .filter(
+                (fish) =>
+                  fish.Season.includes(curSeason) ||
+                  fish.Season.includes("All Seasons")
+              )
+              .map((fish) => (
+                <div key={fish.Name}>{fish.Name}</div> // Render the fish names
+              ))}
+          </div>
+          <button
+            onClick={() => {
+              const newCount = count < 3 ? count + 1 : 0;
+              setCount(newCount);
+              setCurSeason(allSeasons[newCount]);
+            }}
+          >
+            Click me
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Interface;
