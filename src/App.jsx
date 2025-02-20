680d255c8fef448e0f7d92b3b4a7c1938150aaa9
rpfar <rpfarish@live.com>
2025-02-20 00:11:49 -0500

    added wrapper

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,13 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import GridLayout from "./GridLayout";
 import Footer from "./Footer";
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <GridLayout />
+      <div className="wrapper">
+        <GridLayout />
+      </div>
       <Footer />
     </>
   );
