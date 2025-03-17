5a433493acd3371557fa968fb5bf72ffcb8ce878
rpfar <rpfarish@live.com>
2025-03-17 19:15:38 -0400

    fixing deploy gitconfig

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/stardew_fishing/",
+  // npm run build
+  // npm run deploy
   server: {
     host: "0.0.0.0", // Allows access from inside Docker
     port: 5173, // Matches Docker's exposed port
